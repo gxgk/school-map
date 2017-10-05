1dd031a124be45ac53e89ad256c41609ea81af96
dairoot <623815825@qq.com>
2017-10-05 12:09:35 +0800

    del

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  debug: true,
-  school: "gdst",
-  //高德路线规划秘钥，必须加入https://restapi.amap.com为request合法域名
-  key: "2b9a90d2d9ac9ab1902ec83603b144a4", 
-  updateUrl: "http://qn.gxgk.cc/school.js?@21111"
-}
