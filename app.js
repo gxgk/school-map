381e37f7cb036a8a271056cab0f030858d1b0ff8
dairoot <623815825@qq.com>
2018-10-09 11:59:11 +0800

    使用Promise加载异步数据

--- app.js
+++ app.js
@@ -23,58 +23,65 @@ App({
     })
   },
 
+  loadNetWorkScoolConf: function() {
+    var _this = this
+    return new Promise(function(resolve, reject) {
+      if (!_this.debug) {
+        // 优先读取缓存信息
+        var map = wx.getStorageSync('map')
+        var introduce = wx.getStorageSync('introduce')
+        if (map && introduce) {
+          _this.globalData.map = map;
+          _this.globalData.introduce = introduce;
+        }
+
+        // 再加载网络数据
+        wx.request({
+          url: config.updateUrl,
+          header: {
+            'content-type': 'application/json'
+          },
+          success: function(res) {
+            console.log("加载远程数据")
+            if (res.data.map && res.data.map.length > 0) {
+              //刷新数据
+              _this.globalData.map = res.data.map;
+              _this.globalData.introduce = res.data.introduce;
+
+              // 存储学校位置数据于缓存中
+              wx.setStorage({
+                key: "map",
+                data: res.data.map
+              })
+              wx.setStorage({
+                key: "introduce",
+                data: res.data.introduce
+              })
+            }
+          },
+          complete: function(info) {
+            resolve();
+          }
+        })
+      } else {
+        resolve();
+      }
+    });
+  },
+
   loadSchoolConf: function() {
     var _this = this
     // 载入本地数据
     _this.globalData.map = _this.school.map;
     _this.globalData.introduce = _this.school.introduce;
-
-
-    if (!this.debug) {
-      // 优先读取缓存信息
-      var map = wx.getStorageSync('map')
-      var introduce = wx.getStorageSync('introduce')
-      if (map && introduce) {
-        _this.globalData.map = map;
-        _this.globalData.introduce = introduce;
-      }
-
-
-      // 再加载网络数据
-      wx.request({
-        url: config.updateUrl,
-        header: {
-          'content-type': 'application/json'
-        },
-        success: function(res) {
-          console.log("加载远程数据")
-          if (res.data.map && res.data.map.length > 0) {
-            //刷新数据
-            _this.globalData.map = res.data.map;
-            _this.globalData.introduce = res.data.introduce;
-
-            // 存储学校位置数据于缓存中
-            wx.setStorage({
-              key: "map",
-              data: res.data.map
-            })
-            wx.setStorage({
-              key: "introduce",
-              data: res.data.introduce
-            })
-          }
-        },
-        complete: function() {
-          // 渲染id
-          for (let i = 0; i < _this.globalData.map.length; i++) {
-            for (let b = 0; b < _this.globalData.map[i].data.length; b++) {
-              _this.globalData.map[i].data[b].id = b + 1;
-            }
-          }
+    _this.loadNetWorkScoolConf().then(function() {
+      // 渲染id
+      for (let i = 0; i < _this.globalData.map.length; i++) {
+        for (let b = 0; b < _this.globalData.map[i].data.length; b++) {
+          _this.globalData.map[i].data[b].id = b + 1;
         }
-      })
-
-    }
+      }
+    })
 
   },
   debug: config.debug, //开启后只调用本地数据
